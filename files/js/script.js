4e5f1cef3c9185239f4b567c3563f8c444f1403a
arminamirinasab <arminamirinasab@gmail.com>
2022-11-05 22:30:08 +0330

    Add tab switching animation

---
+++ files/js/script.js
@@ -0,0 +1,7 @@
+// Changes Tabs
+
+const homeBorder = document.querySelector("#home-border");
+function changeTabs(position , width = "40px") {
+homeBorder.style.left = position;
+homeBorder.style.width = width;
+}
