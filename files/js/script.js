d118eef22142d97530194c9187e8d2665c377770
arminamirinasab <arminamirinasab@gmail.com>
2022-11-13 17:17:03 +0330

    Add Loading Section

--- files/js/script.js
+++ files/js/script.js
@@ -1,10 +1,18 @@
-// Variables
-const tabBorder = document.querySelector("#home-border");
-const zoomRange = document.querySelector("#zoomRange") , indicateZoom = document.querySelector("#indicateZoom");
-const Sheet = document.querySelector("#Sheet");
-const zoomMinus = document.querySelector("#zoomMinus") , zoomPlus = document.querySelector("#zoomPlus");
+// Hidden Loading Section
+
+const loadingSection = document.getElementById("loadingSection");
+const loadingSectionTransitionTime = 700;
+loadingSection.style.transitionDuration = `${loadingSectionTransitionTime / 1000}s`;
+window.onload = function () {
+  loadingSection.style.opacity = "0";
+  setTimeout(function () {
+    loadingSection.style.display = "none";
+  }, loadingSectionTransitionTime);
+};
 
 // Switch Between Tabs
+
+const tabBorder = document.querySelector("#home-border");
 function changeTabs(position, width = "40px") {
   tabBorder.style.left = position;
   tabBorder.style.width = width;
@@ -12,20 +20,24 @@ function changeTabs(position, width = "40px") {
 
 // Zoom In Sheet
 
-zoomRange.addEventListener("click" , function(e) {
-    changeZoomButton(e.target.value);
-})
+const zoomRange = document.querySelector("#zoomRange"),
+  indicateZoom = document.querySelector("#indicateZoom");
+const Sheet = document.querySelector("#Sheet");
+const zoomMinus = document.querySelector("#zoomMinus"),
+  zoomPlus = document.querySelector("#zoomPlus");
 
-// Changing Zoom Function
+zoomRange.addEventListener("click", function (e) {
+  changeZoomButton(e.target.value);
+});
 
-function changeZoomButton(changeRange , isPlus) {
-    if (isPlus) {
-        changeRangeConverted = Number(zoomRange.value) + changeRange;
-    } else {
-        changeRangeConverted = changeRange;
-    }
+function changeZoomButton(changeRange, isPlus) {
+  if (isPlus) {
+    changeRangeConverted = Number(zoomRange.value) + changeRange;
+  } else {
+    changeRangeConverted = changeRange;
+  }
 
-    indicateZoom.innerHTML = `${Math.round(changeRangeConverted * 100)}%`;
-    Sheet.style.transform = `scale(${changeRangeConverted})`;
-    zoomRange.setAttribute("value" , changeRangeConverted);
+  indicateZoom.innerHTML = `${Math.round(changeRangeConverted * 100)}%`;
+  Sheet.style.transform = `scale(${changeRangeConverted})`;
+  zoomRange.setAttribute("value", changeRangeConverted);
 }
